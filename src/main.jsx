fcf43803f27a9435295a24c8e0856debee7ba831
Forge <28455546+forge34@users.noreply.github.com>
2024-06-20 01:42:27 +0300

    Initial setup comit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import routes from "./router";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={routes}></RouterProvider>{" "}
+  </React.StrictMode>,
+);
